6299289eb65e4e92d208ae8a30d11d3e20d7c96d
kang <1115610574@qq.com>
2021-09-09 08:53:24 +0800

    refactor: zsh structure
    - ~/.zshrc to ~/.config/zsh/.zshrc

---
+++ zsh/.config/zsh/alias.zsh
@@ -0,0 +1,19 @@
+# software
+alias ls="exa --icons --git"
+alias vim=nvim
+alias ra="ranger"
+alias ncdu="ncdu --color dark"
+alias mux="tmuxinator"
+alias s=neofetch
+alias p=proxychains4
+alias cman="man -M /usr/share/man/zh_CN"
+
+# git
+alias ga='git add -A'
+alias gs='git status -s'
+alias gl="git log --pretty=\"%C(yellow)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\" --graph"
+alias gla="git log --pretty=\"%C(yellow)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\" --graph --all"
+alias gd='git diff'
+alias gda='git diff --cached'
+
+alias cg="curl google.com"
