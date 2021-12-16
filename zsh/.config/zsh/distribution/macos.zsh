dc8c4b8ae9a90e83056f3632b036656406ab342a
kang <yi1115610574@gmail.com>
2021-12-16 11:40:31 +0800

    Add gnu-sed and epic-games in Brewfile && set gnu-sed in PATH

--- zsh/.config/zsh/distribution/macos.zsh
+++ zsh/.config/zsh/distribution/macos.zsh
@@ -5,6 +5,8 @@ export proxy_socks5_port=1080
 
 # setting PATH
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+# set GNU sed, not use macOS sed
+PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
 
 # export custom variable
 export is_mac=true
