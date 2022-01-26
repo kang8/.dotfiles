de5a07ef56b91010161623204f758564a44cbeee
kang <yi1115610574@gmail.com>
2022-01-26 23:27:24 +0800

    Bind edit-command-line with insert and normal mode
    - also see: https://stackoverflow.com/questions/11478673/how-to-show-zsh-function-definition-like-bash-type-myfunc#11478720
      to how to display `edit-command-line` function

--- zsh/.config/zsh/key-bindings.zsh
+++ zsh/.config/zsh/key-bindings.zsh
@@ -2,3 +2,6 @@
 bindkey '^ ' autosuggest-accept # contrl + space 填充历史命令
 bindkey -s '^o' 'ranger^M' # contrl + o
 bindkey -s '^t' 'tmux a^M' # contrl + o
+
+bindkey '\C-x\C-e' edit-command-line
+bindkey -M vicmd 'm' edit-command-line
