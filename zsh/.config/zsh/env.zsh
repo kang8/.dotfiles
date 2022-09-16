6e434592b0ca2ecba8b0e608715b3f962a22e660
kang <yi1115610574@gmail.com>
2022-09-16 18:05:37 +0800

    Update zsh history setting

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -11,21 +11,15 @@ export PATH=$PATH:~/.local/bin # customize binary file and python binary file
 export PATH=$PATH:~/go/bin # go binary file
 
 # history
-[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=500000
-[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=100000
+export HISTFILE="$HOME/.zsh_history"
+export HISTSIZE=1000000000
+export HISTFILESIZE=1000000000
 
-# Allow multiple terminal sessions to all append to one zsh command history
-setopt append_history
-setopt extended_history       # record timestamp of command in HISTFILE
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt hist_verify            # show command with history expansion to user before running it
-setopt INC_APPEND_HISTORY # append into history file
+# man zshoptions or man zshparam
+setopt INC_APPEND_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_REDUCE_BLANKS  ## Delete empty lines from history file
-setopt HIST_NO_STORE  ## Do not add history and fc commands to the history
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_REDUCE_BLANKS
 
 # customize variable
 if [[ -n `grep WORK=true ~/.dotfiles/.env` ]]; then
