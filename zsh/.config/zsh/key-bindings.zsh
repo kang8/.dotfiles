28f24120f4215f22209a2a0d9badd3b9dff0d1fd
kang <yi1115610574@gmail.com>
2022-09-16 08:48:24 +0800

    Delete binding for `control+t` to map `tmux a<CR>`

--- zsh/.config/zsh/key-bindings.zsh
+++ zsh/.config/zsh/key-bindings.zsh
@@ -1,7 +1,6 @@
 # zsh-autosuggestions config
 bindkey '^ ' autosuggest-accept # control + space 填充历史命令
 bindkey -s '^o' 'ranger^M'
-bindkey -s '^t' 'tmux a^M'
 bindkey -s '^f' 'fork^M'
 
 bindkey '\C-x\C-e' edit-command-line
