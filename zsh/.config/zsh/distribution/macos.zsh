3f0790a1c3ce2ec8d17452e252bc9a3bdfb9df2f
kang <yi1115610574@gmail.com>
2022-09-23 10:43:57 +0800

    Use Preview.app open man page
    Copy from: https://github.com/donatj/dotfiles/blob/35fbd7d9e087f2372c2af9d448b7e32f8963bb00/zsh/.darwin.zshrc#L5-L16

--- zsh/.config/zsh/distribution/macos.zsh
+++ zsh/.config/zsh/distribution/macos.zsh
@@ -5,3 +5,14 @@ export proxy_socks5_port=1080
 
 # export custom variable
 export is_mac=true
+
+function manp {
+    local page
+    if (( $# > 0 )); then
+        for page in "$@"; do
+            man -t "$page" | open -f -a Preview
+        done
+    else
+        print 'What manual page do you want?' >&2
+    fi
+}
--- zsh/.config/zsh/distribution/setup.zsh
+++ zsh/.config/zsh/distribution/setup.zsh
@@ -1,11 +1,11 @@
 current_dir=`dirname $0`
 
 if [[ -n `type sw_vers &> /dev/null && exec sw_vers` ]]; then
-    . $current_dir/macos.zsh
+    source $current_dir/macos.zsh
 elif [[ -n `cat /proc/version | grep -i wsl2` ]]; then
-    . $current_dir/wsl2.zsh
+    source $current_dir/wsl2.zsh
 elif [[ -n `cat /proc/version | grep -i arch` ]]; then
-    . $current_dir/archlinux.zsh
+    source $current_dir/archlinux.zsh
 else
     echo "Can't find Linux distribution!!!"
 fi
