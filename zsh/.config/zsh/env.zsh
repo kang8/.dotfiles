36c1a225778f14a862de6f74e17e6eba213d7894
kang <yi1115610574@gmail.com>
2022-10-04 10:32:47 +0800

    Configure zsh, source form https://github.com/goreliu/zshguide/wiki/.zshrc

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -20,13 +20,17 @@ export PATH=$PATH:~/go/bin # go binary file
 # history
 export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE=1000000000
+export SAVEHIST=1000000000
 export HISTFILESIZE=1000000000
 
 # man zshoptions or man zshparam
+setopt SHARE_HISTORY
 setopt INC_APPEND_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
 setopt HIST_SAVE_NO_DUPS
 setopt HIST_REDUCE_BLANKS
+setopt PUSHD_IGNORE_DUPS
 
 # customize variable
 if [[ -n `grep WORK=true ~/.dotfiles/.env` ]]; then
