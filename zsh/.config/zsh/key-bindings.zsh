2eaf95906e852f8b46e9b18e0d499f9f04ffd92d
kang <yi1115610574@gmail.com>
2023-01-09 10:14:38 +0800

    Keep emacs key-bindings, open fork change to <c-k>

--- zsh/.config/zsh/key-bindings.zsh
+++ zsh/.config/zsh/key-bindings.zsh
@@ -1,7 +1,9 @@
 # zsh-autosuggestions config
 bindkey '^ ' autosuggest-accept # control + space 填充历史命令
+bindkey '^f' forward-char
+bindkey '^b' backward-char
 bindkey -s '^o' 'ranger^M'
-bindkey -s '^f' 'fork^M'
+bindkey -s '^k' 'fork^M'
 
 bindkey '\C-x\C-e' edit-command-line
 bindkey -M vicmd 'm' edit-command-line
