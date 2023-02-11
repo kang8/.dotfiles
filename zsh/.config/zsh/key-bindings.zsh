51fa2922963de8fad96f2d6e03587cd3f708b1bb
kang <yi1115610574@gmail.com>
2023-02-11 11:49:50 +0800

    Use ripgrep and fzf to search text in terminal and bind `<C-S-f>` to trigger it.

--- zsh/.config/zsh/key-bindings.zsh
+++ zsh/.config/zsh/key-bindings.zsh
@@ -4,6 +4,7 @@ bindkey '^f' forward-char
 bindkey '^b' backward-char
 bindkey -s '^o' 'ranger^M'
 bindkey -s '^k' 'fork^M'
+bindkey -s '^[[102;6u' 'search_text^M' # <C-S-f>
 
 bindkey '\C-x\C-e' edit-command-line
 bindkey -M vicmd 'm' edit-command-line
