3f3880d5e45ac2aabc1f6e6774fba5f04b452c2f
kang <yi1115610574@gmail.com>
2023-06-03 11:56:44 +0800

    Use `bat` to highlight text file in `ranger`

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -301,12 +301,7 @@ handle_mime() {
                 local pygmentize_format='terminal'
                 local highlight_format='ansi'
             fi
-            env HIGHLIGHT_OPTIONS="${HIGHLIGHT_OPTIONS}" highlight \
-                --out-format="${highlight_format}" \
-                --force -- "${FILE_PATH}" && exit 5
-            env COLORTERM=8bit bat --color=always --style="plain" \
-                -- "${FILE_PATH}" && exit 5
-            pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
+            env COLORTERM=8bit bat --color=always --style="numbers,changes" \
                 -- "${FILE_PATH}" && exit 5
             exit 2;;
 
