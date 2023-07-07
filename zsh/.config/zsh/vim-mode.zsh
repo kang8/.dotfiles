973ea5a51945f699664f019d2c212ddb09abe62c
kang <yi1115610574@gmail.com>
2023-07-07 15:35:48 +0800

    Add text objects in vim-mode of zsh

--- zsh/.config/zsh/vim-mode.zsh
+++ zsh/.config/zsh/vim-mode.zsh
@@ -29,3 +29,16 @@ cursor_mode() {
 }
 
 cursor_mode
+
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for km in viopp visual; do
+  bindkey -M $km -- '-' vi-up-line-or-history
+  for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
+    bindkey -M $km $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $km $c select-bracketed
+  done
+done
