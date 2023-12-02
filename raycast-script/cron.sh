d2b4add094a7f6e80d8ddbc84dbac7d9348bd002
kang <yi1115610574@gmail.com>
2023-12-02 20:01:47 +0800

    feat(raycast-link): Add two very useful links

---
+++ raycast-script/cron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Cron
+# @raycast.mode silent
+# @raycast.description Simple editor for cron schedule expressions
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon 🇨
+
+open 'https://crontab.guru/'
---
+++ raycast-script/timezone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Timezone
+# @raycast.mode silent
+# @raycast.description Time Converter and World Clock
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon https://www.worldtimebuddy.com/favicon.ico
+
+open 'https://www.worldtimebuddy.com'
