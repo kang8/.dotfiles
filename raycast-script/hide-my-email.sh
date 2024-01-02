ff8e3c11b45f0c099dd8d7098e83d5f8919bd43b
kang <yi1115610574@gmail.com>
2024-01-02 17:57:17 +0800

    feat(raycast-link): Quick open apple iCloud 'Hide my Email' page

---
+++ raycast-script/hide-my-email.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Hide my Email
+# @raycast.mode silent
+# @raycast.description Open Apple iCloud Hide my Email page
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon 📧
+
+open 'https://www.icloud.com.cn/icloudplus/'
