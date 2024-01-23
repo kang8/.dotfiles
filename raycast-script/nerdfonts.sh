3dc041b54d07e773ba28057fdb5b4b3a98c4e1c5
kang <yi1115610574@gmail.com>
2024-01-23 11:46:36 +0800

    feat(raycast): Quick open nerdfonts cheat-sheet page

---
+++ raycast-script/nerdfonts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Nerd Fonts cheat-sheet page
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon https://www.nerdfonts.com/assets/img/nerd-fonts-logo.svg
+#
+# Credits
+# @raycast.author kang8
+
+open https://www.nerdfonts.com/cheat-sheet
+
