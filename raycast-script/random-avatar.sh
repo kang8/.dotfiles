f0970c5bfe29b59255f751a5c9d01bf535d2479a
kang <yi1115610574@gmail.com>
2024-03-10 11:59:14 +0800

    feat(raycast-script): Add 'Generate random name' and 'Generate random avatar' link

---
+++ raycast-script/random-avatar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Random Avatar Generator
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon 🔀
+
+open 'https://thismountaindoesnotexist.com/'
