25d266ca98934033387e2757805a2a72cc1b6202
kang <yi1115610574@gmail.com>
2024-06-23 15:53:19 +0800

    Adjust my-run-scheme for auto load current file if it's a `.scm` file

--- emacs/.emacs.d/lisp/init-scheme.el
+++ emacs/.emacs.d/lisp/init-scheme.el
@@ -7,9 +7,15 @@
          (preserve-size . (nil . t)))))
 
 (defun my-run-scheme ()
-  "Run Scheme in a new side window, keeping cursor in the original window."
+  "Run Scheme in a new side window and load the current file if it is a '.scm'
+  file, then keeping cursor in the original window"
   (interactive)
-  (run-scheme scheme-program-name)
-  (other-window -1))
+  (unless (get-buffer "*scheme*")
+    (run-scheme scheme-program-name)
+    (other-window -1))
+  (let ((file (buffer-file-name)))
+    (when (and file (string-match "\\.scm\\'" file))
+      (comint-send-string (get-buffer-process "*scheme*")
+                          (concat "(load \"" file "\" )\n")))))
 
 (global-set-key (kbd "C-c s") 'my-run-scheme)
