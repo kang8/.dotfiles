14b2376dc0cb6886e04dfb120dc7d70df4db8d37
kang <yi1115610574@gmail.com>
2024-06-24 11:24:01 +0800

    feat(emacs): Change cursor when change mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,3 +3,16 @@
 (load "init-scheme")
 (load "ui")
 (load "programming")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-terminal-cursor-changer catppuccin-theme spaceline evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs/.emacs.d/lisp/package.el
+++ emacs/.emacs.d/lisp/package.el
@@ -6,3 +6,13 @@
 (require 'evil)
 (evil-mode 1)
 
+(unless (display-graphic-p)
+  (require 'evil-terminal-cursor-changer)
+  (evil-terminal-cursor-changer-activate)
+
+  (setq evil-motion-state-cursor 'box)  ; █
+  (setq evil-visual-state-cursor 'box)  ; █
+  (setq evil-normal-state-cursor 'box)  ; █
+  (setq evil-insert-state-cursor 'bar)  ; ⎸
+  (setq evil-emacs-state-cursor  'hbar) ; _
+  )
