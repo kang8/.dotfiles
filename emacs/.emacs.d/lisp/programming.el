9e9607f66040ca26ef501cbcdbbf496b0c812e60
kang <yi1115610574@gmail.com>
2024-06-24 10:45:32 +0800

    feat(emacs): Integrate Emacs copy/paste with System copy/paste

--- emacs/.emacs.d/lisp/programming.el
+++ emacs/.emacs.d/lisp/programming.el
@@ -1,2 +1,10 @@
 ; Auto-complete the other half of the parentheses
 (electric-pair-mode 1)
+
+(unless window-system
+  (setq interprogram-cut-function
+        (lambda (text &optional push)
+          (let* ((process-connection-type nil)
+                 (pbproxy (start-process "pbcopy" "pbcopy" "/usr/bin/pbcopy")))
+            (process-send-string pbproxy text)
+            (process-send-eof pbproxy)))))
