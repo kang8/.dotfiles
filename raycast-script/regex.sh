7bce46649dd24e086eb1a3da1d1c3e6f4168f1f0
kang <yi1115610574@gmail.com>
2024-12-06 16:16:41 +0800

    raycast: Quick to open regex101.com

---
+++ raycast-script/regex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Regex 101
+# @raycast.mode silent
+# @raycast.description Regular expression tester with syntax highlighting, explanation, cheat sheet
+
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon 🔎
+
+open 'https://regex101.com/'
