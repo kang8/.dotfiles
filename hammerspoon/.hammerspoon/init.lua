9c863f367541b41ffab2f389f6c8aaa25d050e25
kang <yi1115610574@gmail.com>
2024-12-24 22:08:20 +0800

    Auto install spoon package and use InputSourceSwitch spoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,24 @@
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall
+
+-- Auto reload config on change
+-- https://www.hammerspoon.org/Spoons/SpoonInstall.html
+Install:andUse("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+hs.alert.show("Config loaded")
+
+-- https://www.hammerspoon.org/Spoons/InputSourceSwitch.html
+Install:andUse("InputSourceSwitch")
+spoon.InputSourceSwitch:setApplications({
+    ["kitty"] = "ABC",
+    ["PyCharm"] = "ABC",
+    ["WebStorm"] = "ABC",
+    ["Typing Mind"] = "Squirrel - Simplified",
+})
+spoon.InputSourceSwitch:start()
+
+-- Other
+hs.hotkey.bind({"alt"}, "w", function()
+  hs.alert.show(hs.application.frontmostApplication())
+  print(hs.application.frontmostApplication():name())
+end)
