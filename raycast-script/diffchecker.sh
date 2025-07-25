201d26ad8a6ae24b2cf387c1cbbb4d9853218e4a
kang <yi1115610574@gmail.com>
2025-07-25 10:22:19 +0800

    feat(raycast): Quick link for diffchecker

---
+++ raycast-script/diffchecker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Diffchecker
+# @raycast.mode silent
+# @raycast.description Find the difference between two text files
+#
+# Optional parameters:
+# @raycast.packageName Customized
+# @raycast.icon 🆚
+
+open 'https://www.diffchecker.com/'
