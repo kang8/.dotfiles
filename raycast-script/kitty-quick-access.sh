b6733a734f428ca05a7ae95e324d5a93a98777c1
kang <yi1115610574@gmail.com>
2026-05-29 16:33:07 +0800

    feat(raycast): Add kitty quick-access toggle script

---
+++ raycast-script/kitty-quick-access.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Kitty Quick Access
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Kitty
+# @raycast.icon 🐱
+#
+# Documentation:
+# @raycast.description Toggle kitty quick-access-terminal
+#
+# Credits
+# @raycast.author kang
+# @raycast.authorURL https://github.com/kang8
+
+#####################################################################################
+
+/opt/homebrew/bin/kitten quick-access-terminal
